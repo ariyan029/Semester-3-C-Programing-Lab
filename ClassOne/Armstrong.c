8c6c2aefd92acb48fa542ca4a59ad292c02ed09d
Sk Ariyan Ali <murselim49@gmail.com>
2025-09-13 16:55:04 +0000

    push

--- ClassOne/Armstrong.c
+++ ClassOne/Armstrong.c
@@ -27,7 +27,7 @@ int main() {
     // Check if the number is an Armstrong number
     if (result == num)
         printf("%d is an Armstrong number.\n", num);
-    else
+    else    
         printf("%d is not an Armstrong number.\n", num);
 
     return 0;
